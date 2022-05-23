59a33b1d6822bb209f4e086d6aa777cfeb9a913c
Alan Torquatro de Matos <mr.alanmatos@hotmail.com>
2022-05-23 15:52:43 -0400

    commit tree 861e905303ddb64bc3da5a093c62698f93f30ffb

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+
+// test 1
+findMatching = (arr, driverName) => {return arr.filter( driverSearch =>  driverSearch.toLowerCase() === driverName.toLowerCase())}
+
+
+
+
+// test 2
+function fuzzyMatch(drivers, string) {
+
+    const filtered = string.length;
+    return drivers.filter(function (driverName) {
+        return driverName.slice(0, filtered) === string
+
+
+    }
+    )
+}
+
+
+
+// test 3 
+function matchName(arr, driverName) {
+    return arr.filter(newFunc => newFunc.name.toLowerCase() === driverName.toLowerCase());
+}
+
